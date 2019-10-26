e54abbcbd49f8fb018726ab1f800c935796fa7bb
Alexey Dolgikh <adolgich@arkadium.com>
2019-10-26 16:15:22 +0300

    c1w5_final. Done

--- c1w5_final/src/c1w5_final.cpp
+++ c1w5_final/src/c1w5_final.cpp
@@ -11,6 +11,10 @@ using namespace std;
 
 class Date {
 public:
+	Date (string& str_date) {
+		Parse(str_date);
+	}
+
   int GetYear() const {
 	  return year;
   }
@@ -19,28 +23,6 @@ public:
   }
   int GetDay() const {
 	  return day;
-  }
-
-  // "year-month-day" (string)
-  friend istream& operator >> (istream& stream, Date& date) {
-
-	  // Wrong date format: DATE
-
-  	stream >> date.year;
-  	stream.ignore(1); // check that it is particullarly '-'
-
-  	stream >> date.month;
-  	//Month value is invalid: MONTH
-  	// string error = "Month value is invalid: " + to_string(month);
-
-  	stream.ignore(1);
-  	stream >> date.day;
-  	// Day value is invalid: DAY
-
-//	if () {
-//		throw invalid_argument("...");
-//	}
-  	return stream;
   }
 
   friend ostream& operator << (ostream& stream, const Date& date) {
@@ -64,19 +46,48 @@ private:
   int year;
   int month;
   int day;
-};
 
+  void EnsureNextSymbol (istream& stream, const string& str_date) {
+  	if (stream.peek() != '-') {
+  		throw invalid_argument("Wrong date format: " + str_date);
+  	}
+  	stream.ignore(1);
+  }
+
+	void EnsureMonth () {
+		if (month < 1 || month > 12) {
+			throw invalid_argument("Month value is invalid: " + to_string(month));
+		}
+	}
+
+	void EnsureDay () {
+		if (day < 1 || day > 31) {
+			throw invalid_argument("Day value is invalid: " + to_string(day));
+		}
+	}
+
+	void Parse (string& str_date) {
+		stringstream stream(str_date);
+
+		stream >> year;
+		EnsureNextSymbol(stream, str_date);
+
+		stream >> month;
+		EnsureNextSymbol(stream, str_date);
+		EnsureMonth();
+
+		stream >> day;
+		EnsureDay();
+	}
+};
 
 class Database {
 public:
 	void AddEvent(const Date& date, const string& event) {
-		// Event: "sdvsdvsdvasdvasdvdsfbvdsbdfgbn" (string without space and delimiters), not empty!
 		if (event == "") {
 			return;
 		}
 		data[date].insert(event);
-		// ignore similar event for a one date
-		//cout << data.size() << " " << data[date].size() << endl;
 	}
 
 	bool DeleteEvent(const Date& date, const string& event) {
@@ -86,11 +97,9 @@ public:
 				if (data[date].size() == 0) {
 					DeleteDate(date);
 				}
-				//cout << data.size() << " " << data[date].size() << endl;
 				return true;
 			}
 		}
-		//cout << data.size() << " " << data[date].size() << endl;
 		return false;
 	}
 
@@ -98,20 +107,16 @@ public:
 		int n = 0;
 		if (data.count(date) > 0) {
 			n = data[date].size();
-			//data[date].clear();
 			data.erase(date);
 		}
 		return n;
 	}
 
 	set<string> Find(const Date& date) const {
-		// is 'set<string>' already sorted alphabetically?
 		return data.at(date);
 	}
 
 	void Print() const {
-		// 'map' must be already sorted! (sort by date)
-		// 'set<string>' must be already sorted? (sort alphabetically)
 		for (const auto& date : data) {
 			for (const auto& event : date.second) {
 				cout << date.first << " " << event << endl;
@@ -123,17 +128,60 @@ private:
 	map<Date, set<string>> data;
 };
 
-void ParseCommand(stringstream& stream, Date& date, string& event) {
-	stream.ignore(1); // or more?
-	stream >> date;
-	//cout << date.GetDay() << endl;
-
-	event = "";
-
+string ReadParameter(stringstream& stream) {
+	string param = "";
 	if (stream.peek() != '\n') {
-		stream.ignore(1); // or more?
-		stream >> event;
-		//cout << event << endl;
+		stream >> param;
+	}
+	return param;
+}
+
+void ProcessCommand (const string& command, Database& db) {
+	string operation;
+	string str_date;
+	string event;
+
+	stringstream stream(command);
+	stream >> operation;
+
+	if (operation == "Add") {
+		str_date = ReadParameter(stream);
+		Date date = Date(str_date);
+		event = ReadParameter(stream);
+		db.AddEvent(date, event);
+
+	} else if (operation == "Del") {
+		str_date = ReadParameter(stream);
+		Date date = Date(str_date);
+		event = ReadParameter(stream);
+
+		if (event != "") {
+			if (db.DeleteEvent(date, event)) {
+				cout << "Deleted successfully" << endl;
+			} else {
+				cout << "Event not found" << endl;
+			}
+		} else {
+			int n = db.DeleteDate(date);
+			cout << "Deleted " << n << " events" << endl;
+		}
+
+	} else if (operation == "Find") {
+		str_date = ReadParameter(stream);
+		Date date = Date(str_date);
+
+		auto events =  db.Find(date);
+		if (events.size() > 0) {
+			for (const auto& e : events) {
+				cout << e << endl;
+			}
+		}
+
+	} else if (operation == "Print") {
+		db.Print();
+
+	} else {
+		cout << "Unknown command: " << operation << endl;
 	}
 }
 
@@ -141,52 +189,15 @@ int main() {
 	Database db;
 	string command;
 
-	string operation;
-	Date date;
-	string event;
-
 	while (getline(cin, command)) {
 		if (command == "") {
 			continue;
 		}
 
-		stringstream stream(command);
-		stream >> operation;
-
-		//cout << operation << endl;
-
-		if (operation == "Add") {
-			ParseCommand(stream, date, event);
-			db.AddEvent(date, event);
-
-		} else if (operation == "Del") {
-			ParseCommand(stream, date, event);
-
-			if (event != "") {
-				if (db.DeleteEvent(date, event)) {
-					cout << "Deleted successfully" << endl;
-				} else {
-					cout << "Event not found" << endl;
-				}
-			} else {
-				int n = db.DeleteDate(date);
-				cout << "Deleted " << n << " events" << endl;
-			}
-
-		} else if (operation == "Find") {
-			ParseCommand(stream, date, event);
-			auto events =  db.Find(date);
-			if (events.size() > 0) {
-				for (const auto& e : events) {
-					cout << e << endl;
-				}
-			}
-
-		} else if (operation == "Print") {
-			db.Print();
-
-		} else {
-			cout << "Unknown command: " << operation << endl;
+		try {
+			ProcessCommand(command, db);
+		} catch (invalid_argument& ex) {
+			cout << ex.what() << endl;
 		}
 	}
 	return 0;
